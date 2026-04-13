d8cf752bfa15dc03f175a0df465bdcee0cdae752
xdsun777 <charmgo@qq.com>
2026-04-13 17:47:18 +0800

    忽略提示

--- src/env-d.ts
+++ src/env-d.ts
@@ -1,6 +1,8 @@
-export {};
+export { };
 declare global {
   interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     __TAURI__: any; // 如果有更具体的类型，请替换 any
+
   }
 }

fe3fddb975d916b21120608656b21983863a16a2
xdsun777 <charmgo@qq.com>
2026-04-17 12:27:17 +0800

    更新文档

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -17,3 +17,8 @@ declare module '@fontsource/*' {
   const content: any;
   export default content;
 }
+declare module '*.svg' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content: any;
+  export default content;
+}

78016119ca7bd43344a4bb8b8c200031371ad6d2
xdsun777 <charmgo@qq.com>
2026-04-17 12:35:32 +0800

    自定义icon组件

---
+++ src/components/common/IconFont.vue
@@ -0,0 +1,15 @@
+<template>
+  <i 
+    class="iconfont" 
+    :class="[`icon-${name}`, $attrs.class]" 
+  />
+</template>
+
+<script setup lang="ts">
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>

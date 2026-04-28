ec965e45d0ed9e28e3ae5e3cfe1fc9a119973e2d
xdsun777 <charmgo@qq.com>
2026-04-28 23:31:50 +0800

    修复bug

--- src/components/common/IconFont.vue
+++ src/components/common/IconFont.vue
@@ -1,9 +1,10 @@
 <template>
-  <img v-if="props.name == 'logo'" :src="logoSvg" class="h-8 w-8" />
-  <i v-else class="iconfont" :class="[`icon-${name}`, $attrs.class]" />
+  <img v-if="props.name == 'logo'" :src="logoSvg" :class="iconClass" />
+  <i v-else class="iconfont" :class="[`icon-${name}`, iconClass]" />
 </template>
 
 <script setup lang="ts">
+import { computed, useAttrs } from 'vue';
 import logoSvg from '@/assets/logo/32x32.svg';
 
 const props = defineProps({
@@ -12,4 +13,7 @@ const props = defineProps({
     required: true
   }
 })
+
+const attrs = useAttrs();
+const iconClass = computed(() => (attrs.class as string) || '');
 </script>
